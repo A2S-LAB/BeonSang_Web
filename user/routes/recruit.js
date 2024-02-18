34eb578244206c766a2d8f5ee5945a112862260e
myjeong19@naver.com <myjeong19@naver.com>
2024-02-18 19:45:34 +0900

    user .env 설정

--- user/routes/recruit.js
+++ user/routes/recruit.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-    res.render('recruit.ejs', { title: 'Express' });
+  res.render('recruit.ejs', {
+    NAVER_MAP_API_KEY: process.env.NAVER_MAP_API_KEY,
+  });
 });
 
 module.exports = router;
